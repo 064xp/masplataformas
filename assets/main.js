c1e62883aef80966d4dc2bfe1b23761b43611829
Paulo <paulozamora2000@gmail.com>
2018-08-31 00:29:21 -0500

    added responsive navbar

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,10 @@
 var nav_hamburger = document.getElementById('nav__hamburger');
 var nav_hamburger_bars = document.getElementById('nav__hamburger--bars');
 var nav_hamburger_x = document.getElementById('nav__hamburger--x');
-nav_hamburger.addEventListener('click', function(){
-  // this.classList.toggle('clicked');
-  nav_hamburger_bars.classList.toggle('invisible');
-  // nav_hamburger_x.classList.toggle('visible');
-  nav_hamburger_x.classList.toggle('visible');
+var navItems = document.querySelector('nav ul');
 
+nav_hamburger.addEventListener('click', function(){
+  nav_hamburger_bars.classList.toggle('invisible');
+  nav_hamburger_x.classList.toggle('visible');
+  navItems.classList.toggle('grow');
 });
