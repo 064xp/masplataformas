488e896e7879b480d40e9fb1dc0296d428e3045c
Paulo <paulozamora2000@gmail.com>
2019-01-14 14:48:37 -0600

    Finished ui for renta section, removed venta in navbar, fixed mobile navbar bug

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+//responsive hamburger navbar
+
 var nav_hamburger = document.getElementById('nav__hamburger');
 var nav_hamburger_bars = document.getElementById('nav__hamburger--bars');
 var nav_hamburger_x = document.getElementById('nav__hamburger--x');
@@ -8,3 +10,39 @@ nav_hamburger.addEventListener('click', function(){
   nav_hamburger_x.classList.toggle('visible');
   navItems.classList.toggle('grow');
 });
+
+
+//calculate cost widget dropdown toggle
+ var widgetToggles = document.querySelectorAll(".picture-card-dropdown-title");
+
+//add click listener
+widgetToggles.forEach(function(el){
+  el.addEventListener("click",function(el){
+
+    if(el.target.nodeName == "svg"){
+      el = el.target.parentNode;
+    }else{
+      el = el.target;
+    }
+
+    el.parentNode.childNodes[3].classList.toggle('hidden');
+    });
+});
+
+
+//cost calculator
+
+const costs = {
+  GS2632:{
+    day: 1000,
+    week: 5000
+  },
+  E400AJN:{
+    day: 2000,
+    week: 9000
+  },
+  Z45:{
+    day: 2200,
+    week: 9900
+  }
+}
