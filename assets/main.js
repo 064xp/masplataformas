e5e37dab472c8137df40f6e27df3c044f569d1bd
Paulo <paulozamora2000@gmail.com>
2019-01-15 13:27:58 -0600

    removed unused styles and js

--- assets/main.js
+++ assets/main.js
@@ -10,39 +10,3 @@ nav_hamburger.addEventListener('click', function(){
   nav_hamburger_x.classList.toggle('visible');
   navItems.classList.toggle('grow');
 });
-
-
-//calculate cost widget dropdown toggle
- var widgetToggles = document.querySelectorAll(".picture-card-dropdown-title");
-
-//add click listener
-widgetToggles.forEach(function(el){
-  el.addEventListener("click",function(el){
-
-    if(el.target.nodeName == "svg"){
-      el = el.target.parentNode;
-    }else{
-      el = el.target;
-    }
-
-    el.parentNode.childNodes[3].classList.toggle('hidden');
-    });
-});
-
-
-//cost calculator
-
-const costs = {
-  GS2632:{
-    day: 1000,
-    week: 5000
-  },
-  E400AJN:{
-    day: 2000,
-    week: 9000
-  },
-  Z45:{
-    day: 2200,
-    week: 9900
-  }
-}
